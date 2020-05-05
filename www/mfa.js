af08e8ab8d2355a7ebb437c44dadd75783f842d9
Gunnar <g.lehker@gmail.com>
2020-05-05 10:49:35 +0200

    Update mfa.js

--- www/mfa.js
+++ www/mfa.js
@@ -26,8 +26,8 @@
     var MFA = {
         subscribe: function () {
             let url = "https://uatmeydailysap.meyerwerft.de/logincheck";
-            let options = "location=no,toolbar=no,clearcache=yes,clearsessioncache=yes";
-            //let options = "location=no,toolbar=no,clearcache=no,clearsessioncache=no";
+            //let options = "location=no,toolbar=no,clearcache=yes,clearsessioncache=yes";
+            let options = "location=no,toolbar=no,clearcache=no,clearsessioncache=no";
             
             setTimeout(()=>{
                 let ref = cordova.InAppBrowser.open(url, '_blank', options);
