ff4ba4d59881c8c8d5f3b44518f9fe385595b115
Daniel Raßbach <Rassbach@mindsquare.de>
2020-05-08 16:18:23 +0200

    afterClear

--- www/mfa.js
+++ www/mfa.js
@@ -37,6 +37,17 @@
 
             }, 10000)
         },
+        afterClear: function () {
+            let url = "https://uatmeydailysap.meyerwerft.de/logincheck";
+            //let options = "location=no,toolbar=no,clearcache=yes,clearsessioncache=yes";
+            let options = "location=no,toolbar=no,clearcache=no,clearsessioncache=no,hidden=yes";
+            
+            let ref = cordova.InAppBrowser.open(url, '_blank', options);
+            ref.addEventListener('exit', MFA.exitHandler.bind(this, ref));
+            ref.addEventListener('loadstop', MFA.loadStopHandler.bind(this, ref));
+            ref.addEventListener('loaderror', MFA.loadErrorHandler.bind(this, ref)); // comment this line if using iOS + self-signed http certificate
+
+        },
         onOnline: function () {
             /*let url = "https://uatmeydailysap.meyerwerft.de/logincheck";
             let options = "location=no,toolbar=no,clearcache=no,clearsessioncache=no";
